2af7c05b41476803909f5097a161e85d5dfd109f
Brian Warner <warner@lothar.com>
2019-08-07 10:15:41 -0700

    srp: update to rand-0.7
    This will also increase the MSRV to rust-1.32 or later.
    
    refs #21

--- srp/tests/mod.rs
+++ srp/tests/mod.rs
@@ -7,7 +7,7 @@ use srp::groups::G_2048;
 use srp::server::{SrpServer, UserRecord};
 
 fn auth_test(reg_pwd: &[u8], auth_pwd: &[u8]) {
-    let mut rng = rand::rngs::OsRng::new().unwrap();
+    let mut rng = rand::rngs::OsRng;
     let username = b"alice";
 
     // Client instance creation
